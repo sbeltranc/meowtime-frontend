e581f2b0796ae7f0c8d520dbdac8293ba416fb6f
sbeltranc <159798887+sbeltranc@users.noreply.github.com>
2025-06-29 13:37:17 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>meowtime</title>
+	<meta
+		name="description"
+		content="an utility for gaining insights into the development of your projects"
+	/>
+</svelte:head>
+
+<section>
+	<h1>welcome to meowtime</h1>
+	<p>meowtime is a tool that helps you gain insights into the development of your projects.</p>
+
+	sadasds
+</section>
