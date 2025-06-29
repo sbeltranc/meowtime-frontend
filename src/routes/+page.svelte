d1706ce0af034d0c8f4a35354fff6e42d4fa1a29
sbeltranc <159798887+sbeltranc@users.noreply.github.com>
2025-06-29 13:37:40 -0500

    second commit..?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,4 @@
 	/>
 </svelte:head>
 
-<section>
-	<h1>welcome to meowtime</h1>
-	<p>meowtime is a tool that helps you gain insights into the development of your projects.</p>
-
-	sadasds
-</section>
+<section></section>
